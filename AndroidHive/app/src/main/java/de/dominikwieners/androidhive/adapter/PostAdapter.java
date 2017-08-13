36b089773ce8dd24f4091d08f01b6b72478b6d84
domw94 <dom.wieners@gmx.net>
2017-08-13 03:10:46 +0200

    add RecyclerView and Cards for MainActivity

---
+++ AndroidHive/app/src/main/java/de/dominikwieners/androidhive/adapter/PostAdapter.java
@@ -0,0 +1,8 @@
+package de.dominikwieners.androidhive.adapter;
+
+/**
+ * Created by dominikwieners on 13.08.17.
+ */
+
+public class PostAdapter {
+}
---
+++ AndroidHive/app/src/main/java/de/dominikwieners/androidhive/model/Post.java
@@ -0,0 +1,8 @@
+package de.dominikwieners.androidhive.model;
+
+/**
+ * Created by dominikwieners on 13.08.17.
+ */
+
+public class Post {
+}
