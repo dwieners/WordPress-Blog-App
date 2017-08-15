5d36674f92dd692511c437768015e6ec00af8b19
domw94 <dom.wieners@gmx.net>
2017-08-15 15:45:55 +0200

    no media exist (fix bug)

--- AndroidHive/app/src/main/java/de/dominikwieners/androidhive/app/PostActivity.java
+++ AndroidHive/app/src/main/java/de/dominikwieners/androidhive/app/PostActivity.java
@@ -1,37 +1,23 @@
 package de.dominikwieners.androidhive.app;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BlurMaskFilter;
-import android.graphics.Color;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.google.gson.JsonObject;
-
-import org.w3c.dom.Text;
-
-import java.util.List;
 
 import de.dominikwieners.androidhive.R;
 import de.dominikwieners.androidhive.model.Media;
 import de.dominikwieners.androidhive.util.InternetConnection;
-import jp.wasabeef.glide.transformations.BlurTransformation;
-import jp.wasabeef.glide.transformations.ColorFilterTransformation;
-import jp.wasabeef.glide.transformations.GrayscaleTransformation;
-import jp.wasabeef.glide.transformations.MaskTransformation;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -67,32 +53,10 @@ public class PostActivity extends AppCompatActivity {
         String content = getIntent().getSerializableExtra("de.dominikwieners.androidhive.postContent").toString();
 
 
-        //Set Toolbar
-        postToolbar = (Toolbar) findViewById(R.id.postToolbar);
-        setSupportActionBar(postToolbar);
-        initCollapsingToolbar(title);
+        initToolbar(title);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initPost(title, content);
 
-        postToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        //Set TitleText and ContentText
-        postTitle = (TextView) findViewById(R.id.post_title);
-        postTitle.setText(title);
-
-        /*
-        postContent = (TextView) findViewById(R.id.post_content);
-        postContent.setText(content);
-        */
-
-
-
-        postBackdrop = (ImageView) findViewById(R.id.post_backdrop) ;
 
        //Call Media
         if(InternetConnection.checkInternetConnection(getApplicationContext())) {
@@ -106,14 +70,20 @@ public class PostActivity extends AppCompatActivity {
                 public void onResponse(Call<Media> call, Response<Media> response) {
 
                     Log.d("ResponseMediaCode", "Status = " + response.code());
-                    Media media = response.body();
-                    String mediaUrl = media.getGuid().get("rendered").toString().replaceAll("\"", "");
 
-                    Glide.with(getApplicationContext()).load(mediaUrl)
-                            .thumbnail(0.5f)
-                            .crossFade()
-                            .diskCacheStrategy(DiskCacheStrategy.ALL)
-                            .into(postBackdrop);
+                    if (response.code() != 404) {
+                        Media media = response.body();
+                        String mediaUrl = media.getGuid().get("rendered").toString().replaceAll("\"", "");
+
+                        Glide.with(getApplicationContext()).load(mediaUrl)
+                                .thumbnail(0.5f)
+                                .crossFade()
+                                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                                .into(postBackdrop);
+                    }else{
+
+                    }
+
                 }
 
                 @Override
@@ -125,10 +95,40 @@ public class PostActivity extends AppCompatActivity {
             Snackbar.make(parentView, "Can't connect to the Internet", Snackbar.LENGTH_INDEFINITE);
         }
 
-
-
     }
 
+    //Init Toolbar but do not set media
+    private void initPost(String title, String content ){
+        //Set TitleText and ContentText
+        postTitle = (TextView) findViewById(R.id.post_title);
+        postTitle.setText(title);
+
+        /*
+        postContent = (TextView) findViewById(R.id.post_content);
+        postContent.setText(content);
+        */
+
+        postBackdrop = (ImageView) findViewById(R.id.post_backdrop) ;
+    }
+
+    //Init Toolbar
+    private void initToolbar(String title){
+        //Set Toolbar
+        postToolbar = (Toolbar) findViewById(R.id.postToolbar);
+        setSupportActionBar(postToolbar);
+        initCollapsingToolbar(title);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        postToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    //Init CollapsingToolbarLayout
     private void initCollapsingToolbar(final String title){
         final CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.post_collapsing_toolbarLayout);
